2d6b75359022a3aebff5a92d8881cd25eb828ac7
oliveraluiss11 <loliverv11@gmail.com>
2024-04-23 22:24:47 -0500

    Creacion del proyecto de Kardex Microservice

---
+++ src/main/java/com/jayway/mskardex/MsKardexApplication.java
@@ -0,0 +1,13 @@
+package com.jayway.mskardex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsKardexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsKardexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayway/mskardex/MsKardexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayway.mskardex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsKardexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
